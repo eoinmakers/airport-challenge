cce38bedc1e0798e17556f50aa65c057ac3967db
eoin <eoin@makers.tech>
2020-02-12 17:18:40 +0000

    commented out code removed

--- src/Weather.js
+++ src/Weather.js
@@ -3,10 +3,6 @@ class Weather {
 		this.conditions = ["stormy", "sunny", "sunny", "sunny"];
 	}
 
-	// get forecast() {
-	// 	return this.checkForecast();
-	// }
-
 	getForecast() {
 		return this.conditions[Math.floor(Math.random() * this.conditions.length)];
 	}
